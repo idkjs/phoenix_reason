052f6466f33ae42b13e4339574a49c9d391c12ea
Alain Armand <aarmand.inbox@gmail.com>
2021-03-11 23:38:18 +0100

    brunch/webpack not working

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,83 +1,39 @@
-const { join } = require('path');
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-// PLUGINS
-// =======
-
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-// PATHS
-// =====
-
-const PATHS = {
-  src: join(__dirname, 'src'),
-  static: join(__dirname, 'static'),
-  css: join(__dirname, 'css'),
-  build: join(__dirname, '../priv/static'),
-}
-
-// CONFIG
-// ======
-
-module.exports = {
-  entry: [
-    join(PATHS.src, 'app.re'),
-  ],
+module.exports = (env, options) => ({
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  entry: ["./src/App.bs.js", "./css/app.css"],
   output: {
-    path: PATHS.build,
-    pathinfo: true,
-    filename: 'js/app.js',
-    chunkFilename: 'js/[name].chunk.js',
+    filename: "app.js",
+    path: path.resolve(__dirname, "../priv/static/js")
   },
   module: {
     rules: [
       {
-        exclude: [
-          /\.re$/,
-          /\.rei$/,
-          /\.ml$/,
-          /\.mli$/,
-          /\.css$/,
-          /\.js$/,
-        ],
-        include: PATHS.static,
-        loader: require.resolve('file-loader'),
-      },
-      {
-        test: /\.(re|rei|ml|mli)$/,
-        loader: require.resolve('bs-loader'),
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: require.resolve('style-loader'),
-          use: require.resolve('css-loader'),
-        }),
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          preset: ['es2015'],
-        },
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.re', '.rei', '.ml', '.mli', '.js'],
-    alias: {
-      css: PATHS.css,
-    },
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
   },
   plugins: [
-    new ExtractTextPlugin('css/app.css'),
-    new CopyWebpackPlugin([
-      { from: join(PATHS.static) },
-    ]),
-  ],
-};
+    new MiniCssExtractPlugin({ filename: "../css/app.css" }),
+    new CopyWebpackPlugin([{ from: "static/", to: "../" }])
+  ]
+});
